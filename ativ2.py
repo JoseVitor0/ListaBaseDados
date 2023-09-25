4ba97a732af8bdfd76483c77ccbc1bbfd65fd61e
YAN BENCK BALDISSERA <115998191+YanBaldissera@users.noreply.github.com>
2023-09-25 16:37:42 -0300

    Update ativ2.py

--- ativ2.py
+++ ativ2.py
@@ -1,4 +1,4 @@
-#Bibliotecas utilizadas: pandas, matplot e numpy
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,7 +10,6 @@ df2020 = pd.read_csv("Dados_PRF_2020.csv", encoding="latin-1", sep=";")
 df2021 = pd.read_csv("Dados_PRF_2021.csv", encoding="latin-1", sep=";")
 df2022 = pd.read_csv("Dados_PRF_2022.csv", encoding="latin-1", sep=";")
 
-# Aqui está sendo filtrado nos dados, apenas os acidentes que tiveram relação com ingestão de alcool
 def filtrar_ingestao_alcool(df):
     return df[(df["causa_acidente"] == "Ingestão de álcool pelo condutor") | (df["causa_acidente"] == "Ingestão de Álcool")]
 
@@ -21,7 +20,6 @@ df_alcool2020 = filtrar_ingestao_alcool(df2020)
 df_alcool2021 = filtrar_ingestao_alcool(df2021)
 df_alcool2022 = filtrar_ingestao_alcool(df2022)
 
-# Contando o número total de acidentes relacionados à ingestão de álcool para cada ano
 contagens = [
     len(df_alcool2018),
     len(df_alcool2019),
@@ -32,20 +30,16 @@ contagens = [
 
 anos = [2018, 2019, 2020, 2021, 2022]
 
-#Aqui está sendo criado o gráfico, nesse caso será um grafico de barra
 plt.bar(anos, contagens, color='blue')
 
-#informações exibidas nos eixos do grafico
+
 plt.xlabel('Ano')
 plt.ylabel('Quantidade de Acidentes')
 plt.title('Quantidade Total de Acidentes por Ano - Ingestão de Álcool')
 
-#Configurando para exibir em cada barra o valor de acidentes
 for x, y in zip(anos, contagens):
    plt.text(x, y, str(y), ha='center', va='bottom', fontsize=10, color='blue')
 
-#Definindo para no eixo x exibir cada ano embaixo das barras
 plt.xticks(anos)
 
-#Exibindo o grafico
 plt.show()
